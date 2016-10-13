5393c654d975bde08b4fe9e9e9506ceb7911a1ee
yaohong <231344781@qq.com>
2016-10-13 15:41:55 +0800

    update

--- client_lib/client_lib/main.cpp
+++ client_lib/client_lib/main.cpp
@@ -1,10 +1,11 @@
 // client_lib.cpp : 定义控制台应用程序的入口点。
 //
-#include "stdafx.h"
+#include "client_lib.h"
 
-
-int _tmain( int argc, _TCHAR* argv[] )
+int main( int argc, char* argv[] )
 {
+    client_lib::Socket socket_;
+    bool nRet = socket_.Connect("192.168.1.181", 4576, 1000);
     return 0;
 }
 
