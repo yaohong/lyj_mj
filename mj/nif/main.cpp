6ff63aa5c194dc974c5d77360dbd06c8d8015d5f
姚鸿 <yaohong@iyunyao.com>
2016-10-15 11:13:02 +0800

    add

---
+++ mj/nif/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  lyj_mj
+//
+//  Created by 姚鸿 on 16/10/5.
+//  Copyright © 2016年 姚鸿. All rights reserved.
+//
+
+#include <iostream>
+#include "game_logic.h"
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    MainLogic logic;
+    Init(&logic);
+    
+    return 0;
+}
