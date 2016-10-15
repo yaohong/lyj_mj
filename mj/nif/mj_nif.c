36d5022a89e370ecff74aa390eec0b0bd952c968
yaohong <231344781@qq.com>
2016-10-15 20:14:18 +0800

    update

--- mj/nif/mj_nif.c
+++ mj/nif/mj_nif.c
@@ -5,15 +5,13 @@
 #include "erl_nif.h"
 #include "game_logic.h"
 
-static ERL_NIF_TERM change(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
+static ERL_NIF_TERM test_func(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
 {
 	ErlNifBinary nifBin;
 	enif_inspect_binary(env, argv[0], &nifBin);
 	MainLogic *foo = (MainLogic *)(nifBin.data);
 	
-	int newValue = 0;
-    enif_get_int(env, argv[1], &newValue);
-	return enif_make_atom(env, "success");
+	return enif_make_badarg(env);
 }
 
 
@@ -23,9 +21,8 @@ static ERL_NIF_TERM game_start(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv
 	int brankerNumber = -1;
 	int Ret = enif_alloc_binary(sizeof(MainLogic), &nifBin);
 	printf("enif_alloc_binary %d\n", Ret);
-
-	enif_get_int(env, argv[1], &brankerNumber);
 	MainLogic *foo = (MainLogic *)(nifBin.data);
+	enif_get_int(env, argv[1], &brankerNumber);
 	Init(foo, (int8)brankerNumber);
 	ERL_NIF_TERM binTerm = enif_make_binary(env, &nifBin);
 	return enif_make_tuple(env, 2, enif_make_atom(env, "success"), binTerm);
@@ -33,7 +30,8 @@ static ERL_NIF_TERM game_start(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv
 
 
 static ErlNifFunc nif_funcs[] = {
-	{"game_start", 1, game_start}
+	{"game_start", 1, game_start},
+	{"test_func", 0, test_func}
 };
 
 
--- mj/nif/mj_nif.erl
+++ mj/nif/mj_nif.erl
@@ -2,10 +2,13 @@
 
 
 -export([init/0]).
--export([game_start/1]).
+-export([game_start/1, test_func/0]).
 init() ->
 	erlang:load_nif("./mj_nif", 0).
 
 
 game_start(BankerNumber) ->
+	exception.
+
+test_func() ->
 	exception.
