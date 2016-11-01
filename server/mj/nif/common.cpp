6664864eda90e0ebda1bb20ab3f664ef6086c42c
yaohong <231344781@qq.com>
2016-11-01 11:00:50 +0800

    add

--- server/mj/nif/common.cpp
+++ server/mj/nif/common.cpp
@@ -9,35 +9,39 @@ std::string getPaiString( uint8 p )
     char buff[16] = { 0 };
     if (type == 1)
     {
-        sprintf( buff, "%d-万", value);
+        sprintf( buff, "%d-万.", value );
     }
     else if (type == 2)
     {
-        sprintf( buff, "%d-条", value );
+        sprintf( buff, "%d-条.", value );
     }
     else if (type == 3)
     {
-        sprintf( buff, "%d-筒", value );
+        sprintf( buff, "%d-筒.", value );
     }
     else if (type == 5)
     {
         //中发白
         if (1 == value)
         {
-            sprintf( buff, "红中" );
+            sprintf( buff, "红中." );
         }
         else if (2 == value)
         {
-            sprintf( buff, "发财" );
+            sprintf( buff, "发财." );
         }
         else if (3 == value)
         {
-            sprintf( buff, "白板" );
+            sprintf( buff, "白板." );
         }
         else
         {
             assert( false );
         }
+    }
+    else
+    {
+        assert( false );
     }
     
     
